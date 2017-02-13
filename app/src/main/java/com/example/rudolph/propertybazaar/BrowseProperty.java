e27128f0ad4b22f40efa1f765a22b7f4edf93b41
Rudolph Almeida <rudolf1.almeida@gmail.com>
2017-02-13 23:18:01 +0530

    Fixed BottomNavigationMenuView and removed shift mode from it.

--- app/src/main/java/com/example/rudolph/propertybazaar/BrowseProperty.java
+++ app/src/main/java/com/example/rudolph/propertybazaar/BrowseProperty.java
@@ -1,14 +1,19 @@
 package com.example.rudolph.propertybazaar;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
 
 public class BrowseProperty extends Activity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_property);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bniv_bottom_nav);
+        BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
     }
 }
