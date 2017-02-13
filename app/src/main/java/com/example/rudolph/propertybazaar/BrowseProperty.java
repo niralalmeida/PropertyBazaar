4fbb6daf1a50362d754105708b4daa44f40573b5
Rudolph Almeida <rudolf1.almeida@gmail.com>
2017-02-13 20:29:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/rudolph/propertybazaar/BrowseProperty.java
@@ -0,0 +1,14 @@
+package com.example.rudolph.propertybazaar;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrowseProperty extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_property);
+    }
+}
